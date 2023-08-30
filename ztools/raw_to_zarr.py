54be95f285a15afec8e75debc2309567934b7099
brianreicher <reicher.b@northeastern.edu>
2023-08-30 14:14:21 -0400

    Refactor and add blockwise raw->zarr

--- ztools/raw_to_zarr.py
+++ ztools/raw_to_zarr.py
@@ -1,25 +1,23 @@
 import numpy as np
 from funlib.geometry import Roi
 from funlib.persistence import prepare_ds
-from funlib.geometry import Roi, Coordinate
+from funlib.geometry import Roi
 from funlib.persistence import prepare_ds, Array
 import daisy
 
 
 def save_volume_to_zarr(
-    in_file_path: str = "/n/data3/hms/neurobio/htem/xnh/volraw/s46_V1_100nm_7_q3_rec_.raw",
-    out_file_path: str = "../../data/monkey_xnh.zarr",
-    ds_name: str = "s46_V1_100nm_7_q3_rec",
-    volume_size: list = [3216, 3216, 3216],
-    voxel_size: list = [10, 10, 10],
+    in_file_path: str = "",
+    out_file_path: str = "",
+    ds_name: str = "",
+    volume_size: list[int, int, int] = [1000]*3,
+    voxel_size: list[int, int, int] = [10]*3,
     dtype_in=np.uint8,
     dtype_out=np.uint8,
 ) -> None:
-    total_roi = Roi((0, 0, 0), np.array(volume_size) * np.array(voxel_size))
-
-    read_roi = write_roi = Roi(
-        offset=(0,) * 3, shape=Coordinate((10, 10, 10))
-    ) * Coordinate(voxel_size)
+    
+    # set Zarr total ROI, along with Daisy read/write ROIs
+    total_roi: Roi= Roi(offset=(0, 0, 0), shape=np.array(volume_size) * np.array(voxel_size))
 
     print(f"Opening a zarr volume")
     vol: Array = prepare_ds(
@@ -31,33 +29,76 @@ def save_volume_to_zarr(
         delete=True,
     )
 
-    # --- Load image volume and upload --- #
+    # load raw file into memory and reshape according to the desired Zarr volume size
     print("Loading raw volume into memory...")
-
     im: np.ndarray = np.fromfile(file=in_file_path, dtype=dtype_in)
+
     print("Reshaping...")
     im: np.ndarray = im.reshape(*volume_size, order="F").astype(dtype=dtype_out)
 
-    def zarr_write_worker(
-        block: daisy.Block, im: np.ndarray = im, vol: np.ndarray = vol
+    # check compatible shapes
+    assert vol.shape == im.shape
+
+    # write and save the array to the Zarr
+    print("Saving to Zarr...")
+    vol[total_roi] = im
+    print("Finished saving to Zarr.")
+
+
+def save_volume_to_zarr_blockwise(
+    in_file_path: str = "",
+    out_file_path: str = "",
+    ds_name: str = "",
+    volume_size: list[int, int, int] = [1000]*3,
+    voxel_size: list[int, int, int] = [10]*3,
+    dtype_in=np.uint8,
+    dtype_out=np.uint8,
+) -> bool:
+    # set Zarr total ROI, along with Daisy read/write ROIs
+    total_roi = read_roi = write_roi = Roi((0, 0, 0), np.array(volume_size) * np.array(voxel_size))
+
+    print(f"Opening a zarr volume")
+    vol: Array = prepare_ds(
+        filename=out_file_path,
+        ds_name=ds_name,
+        total_roi=total_roi,
+        voxel_size=voxel_size,
+        dtype=dtype_out,
+        delete=True,
+    )
+
+    # load raw file and reshape according to the desired Zarr volume size
+    print("Loading raw volume into memory...")
+    im: np.ndarray = np.fromfile(file=in_file_path, dtype=dtype_in)
+
+    print("Reshaping...")
+    im: np.ndarray = im.reshape(*volume_size, order="F").astype(dtype=dtype_out)
+
+    # daisy worker to write chunks of the in-memory array to the Zarr
+    def write_worker(
+        block: daisy.Block, im: np.ndarray = im, vol: Array = vol
     ) -> bool:
         im: np.ndarray = im[block.read_roi]
         vol[block.write_roi] = im
         return True
-
+    
+    # check compatible shapes
     assert vol.shape == im.shape
+
+    # create a Daisy distributed task
     task: daisy.Task = daisy.Task(
         task_id="ConvertZarrTask",
         total_roi=total_roi,
         read_roi=read_roi,
         write_roi=write_roi,
-        process_function=zarr_write_worker,
+        process_function=write_worker,
         num_workers=25,
         read_write_conflict=False,
         fit="valid",
     )
 
-    print("Saving to zarr...")
-    # done: bool = daisy.run_blockwise(tasks=[task])
-    vol[total_roi] = im
-    print("Done.")
+    # write and save the array to the Zarr, blockwise
+    print("Saving to Zarr...")
+    done: bool = daisy.run_blockwise(tasks=[task])
+    print("Finished saving Zaarr.")
+    return done
